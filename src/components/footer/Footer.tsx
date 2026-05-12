9068583295359e0d7017b81b94d55537e3a27e2b
chris91300 <52381227+chris91300@users.noreply.github.com>
2026-05-12 14:51:34 +0200

    add: component footer (#19)
    * add: component footer
    
    * update: style
    
    ---------
    
    Co-authored-by: MICHEL Christophe <christophe.michel@externe.maif.fr>

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+
+import SocialMedia from "../SocialMedia/SocialMedia";
+import "./footer.css";
+
+
+export default function Footer(){
+    return(
+        <footer>
+            <SocialMedia />
+        </footer>
+    )
+}
