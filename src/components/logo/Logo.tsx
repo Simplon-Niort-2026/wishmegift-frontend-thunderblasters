249c040ce2b9c426178bc422ceb838185cdd0cf0
LOPEZ Aymeric <aymeric.lopez@maif.fr>
2026-05-12 16:52:22 +0200

    add header component

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,14 @@
+import Image from "../images/Image";
+import Link from "../links/Link";
+
+
+export default function Logo() {
+    return (
+        <Link href="#" title="logo">
+            <Image
+                src="/images/logo.webp" 
+                alt="logo de l'application"
+            />
+        </Link>
+    )
+}
